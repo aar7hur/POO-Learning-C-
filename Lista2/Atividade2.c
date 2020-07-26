efe0ac14e67e43fb5e3d7aa01634cff624b05890
aar7hur <munhozarthur@gmail.com>
2020-07-26 17:49:05 -0300

    Addition, subtracion, multiplication, simplification with rational numbers

--- Lista2/Atividade2.c
+++ Lista2/Atividade2.c
@@ -19,13 +19,8 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <string.h>
-/*********************************************************************
- * 
- *   DEFINES
- * 
- ********************************************************************/
 
-#define MAX_DIGITS 12
+#define MAX_DIGITS 10
 
 /*********************************************************************
  * 
@@ -33,21 +28,13 @@
  * 
  ********************************************************************/
 
-
 typedef struct RationalNumber
 {
     int numerator;
     int denominator;
 } RationalNumber;
 
-typedef RationalNumber *(*Operation)(int a, int b);
-
-
-typedef struct Commands
-{
-    Operation opt;
-} Commands;
-
+RationalNumber *(*operation)(RationalNumber *a, RationalNumber *b);
 
 
 /*********************************************************************
@@ -59,99 +46,114 @@ RationalNumber *addRational(RationalNumber *a, RationalNumber *b);
 RationalNumber *subRational(RationalNumber *a, RationalNumber *b);
 RationalNumber *multiplyRational(RationalNumber *a, RationalNumber *b);
 RationalNumber *divideRational(RationalNumber *a, RationalNumber *b);
-RationalNumber *simplifyRational(RationalNumber *rational, int n);
+RationalNumber *simplifyRational(RationalNumber *rational, RationalNumber *b);
 void *allocateMemory(size_t size);
-RationalNumber *getFromUserInput();
-int readStdinBuffer();
+RationalNumber *getFromUserInput(char *outputString);
+int readStdinBuffer(); 
 int doMMC(int a, int b);
 int doMDC(int a, int b);
 
-/*********************************************************************
- * 
- *   VARIABLES
- * 
- ********************************************************************/
 
 /** Main Function **/
 
 int main()
 {
-    Commands *command = allocateMemory(sizeof *command);
+    
     RationalNumber *aNumber, *bNumber,*add, *sub, *div, *multiply, *simplify;
 
-    aNumber = getFromUserInput();
-    (*command).opt = simplifyRational;
-    simplify = (*command).opt(aNumber, 0);
+    aNumber = getFromUserInput("Rational Number A: \n");
+    bNumber = getFromUserInput("Rational Number B: \n");
+
+
+    operation = simplifyRational;
+    simplify = (*operation)(aNumber, aNumber);
     printf("A = %d/%d", (*aNumber).numerator, (*aNumber).denominator);
     printf("  = %d/%d ", (*simplify).numerator, (*simplify).denominator);
-    printf("  = %f ", (float)(*simplify).numerator / (*simplify).denominator);
+    printf("  = %f \n", (float)(*simplify).numerator / (*simplify).denominator);
     
 
-    bNumber = getFromUserInput();
-    (*command).opt = simplifyRational;
-    simplify = (*command).opt(bNumber, 0);
-    printf("A = %d/%d", (*bNumber).numerator, (*bNumber).denominator);
+    operation = simplifyRational;
+    simplify = (*operation)(bNumber, bNumber);
+    printf("B = %d/%d", (*bNumber).numerator, (*bNumber).denominator);
     printf("  = %d/%d ", (*simplify).numerator, (*simplify).denominator);
-    printf("  = %f ", (float)(*simplify).numerator / (*simplify).denominator);
+    printf("  = %f \n", (float)(*simplify).numerator / (*simplify).denominator);
     
 
-    (*command).opt = addRational;
-    add = (*command).opt(aNumber, bNumber);
-    (*command).opt = simplifyRational;
-    simplify = (*command).opt(add, 0);
+    operation = addRational;
+    add = (*operation)(aNumber, bNumber);
+    operation = simplifyRational;
+    simplify = (*operation)(add, add);
     printf("A + B = %d/%d ", (*add).numerator, (*add).denominator);
     printf("  = %d/%d ", (*simplify).numerator, (*simplify).denominator);
-    printf("  = %f ", (float)(*simplify).numerator / (*simplify).denominator);
+    printf("  = %f \n", (float)(*simplify).numerator / (*simplify).denominator);
 
 
-    (*command).opt = subRational;
-    sub = (*command).opt(aNumber, bNumber);
-    (*command).opt = simplifyRational;
-    simplify = (*command).opt(sub, 0);
+    operation = subRational;
+    sub = (*operation)(aNumber, bNumber);
+    operation = simplifyRational;
+    simplify = (*operation)(sub, sub);
     printf("A - B = %d/%d ", (*sub).numerator, (*sub).denominator);
     printf("  = %d/%d ", (*simplify).numerator, (*simplify).denominator);
-    printf("  = %f ", (float)(*simplify).numerator / (*simplify).denominator);
+    printf("  = %f \n", (float)(*simplify).numerator / (*simplify).denominator);
     
-    
-    (*command).opt = multiplyRational;
-    multiply = (command).opt(aNumber, bNumber);
-    (*command).opt = simplifyRational;
-    simplify = (*command).opt(multiply, 0);
+
+    operation = multiplyRational;
+    multiply = (*operation)(aNumber, bNumber);
+    operation = simplifyRational;
+    simplify = (*operation)(multiply, 0);
     printf("A * B = %d/%d ", (*multiply).numerator, (*multiply).denominator);
     printf("  = %d/%d ", (*simplify).numerator, (*simplify).denominator);
+    printf("  = %f \n", (float)(*simplify).numerator / (*simplify).denominator);
 
 
-    (*command).opt = divideRational;
-    div = (*command).opt(aNumber, bNumber);
-    (*command).opt = simplifyRational;
-    simplify = (*command).opt(div, 0);
-    printf("A * B = %d/%d ", (*div).numerator, (*div).denominator);
+    operation = divideRational;
+    div = (*operation)(aNumber, bNumber);
+    operation = simplifyRational;
+    simplify = (*operation)(div, div);
+    printf("A / B = %d/%d ", (*div).numerator, (*div).denominator);
     printf("  = %d/%d ", (*simplify).numerator, (*simplify).denominator);
+    printf("  = %f \n", (float)(*simplify).numerator / (*simplify).denominator);
 
 }
 
+/*********************************************************************
+ *   Function: addRational
+ * ----------------------
+ * Function that receive two pointers to numbers, adds them and returns
+ * a pointer to memory where the result was allocated.
+ *
+ * returns: a pointer
+ ********************************************************************/
 
 RationalNumber *addRational(RationalNumber *a, RationalNumber *b)
 {   
     int mmc, addNumerator1, addNumerator2;
     RationalNumber *add = allocateMemory(sizeof(*add));
     
-    mmc = doMMC(a, b);
+    mmc = doMMC((*a).denominator,(*b).denominator);
     addNumerator1 = (mmc / (*a).denominator) * (*a).numerator;
     addNumerator2 = (mmc / (*b).denominator) * (*b).numerator;
 
-    (*add).numerator = addNumerator1 + addNumerator2;
+    (*add).numerator = (addNumerator1 + addNumerator2);
     (*add).denominator =  mmc;
 
     return add;
 }
 
+/*********************************************************************
+ *   Function: subRational
+ * ----------------------
+ * Function that receive two pointers to numbers, subtracts them and returns
+ * a pointer to memory where the result was allocated.
+ *
+ * returns: a pointer
+ ********************************************************************/
 RationalNumber *subRational(RationalNumber *a, RationalNumber *b)
 {
     int mmc, addNumerator1, addNumerator2;
     RationalNumber *sub = allocateMemory(sizeof(*sub));
     
-    mmc = doMMC(a, b);
+    mmc = doMMC((*a).denominator, (*b).denominator);
     addNumerator1 = (mmc / (*a).denominator) * (*a).numerator;
     addNumerator2 = (mmc / (*b).denominator) * (*b).numerator;
 
@@ -161,6 +163,15 @@ RationalNumber *subRational(RationalNumber *a, RationalNumber *b)
     return sub;
 }
 
+
+/*********************************************************************
+ *   Function: miltiplyRational
+ * ----------------------
+ * Function that receive two pointers to numbers, multiplies them and returns
+ * a pointer to memory where the result was allocated.
+ *
+ * returns: a pointer
+ *  ********************************************************************/
 RationalNumber *multiplyRational(RationalNumber *a, RationalNumber *b)
 {
     RationalNumber *multiply = allocateMemory(sizeof(*multiply));
@@ -171,19 +182,35 @@ RationalNumber *multiplyRational(RationalNumber *a, RationalNumber *b)
     return multiply;
 }
 
+/*********************************************************************
+ *   Function: divideRational
+ * ----------------------
+ * Function that receive two pointers to numbers, divides them and returns
+ * a pointer to memory where the result was allocated.
+ *
+ * returns: a pointer
+ *  ********************************************************************/
 RationalNumber *divideRational(RationalNumber *a, RationalNumber *b)
 {
     RationalNumber *divide = allocateMemory(sizeof(*divide));
 
-    (*divide).numerator = (*a).numerator * (*b).numerator;
-    (*divide).denominator = (*a).denominator * (*b).denominator;
+    (*divide).numerator = (*a).numerator * (*b).denominator;
+    (*divide).denominator = (*a).denominator * (*b).numerator;
 
     return divide;
 }
 
-RationalNumber *simplifyRational(RationalNumber *rational, int n)
+/*********************************************************************
+ *   Function: simplifyRational
+ * ----------------------
+ * Function that receive a pointer to number, do te simplification and
+ * returns a pointer to memory where the result was allocated.
+ *
+ * returns: a pointer
+ *  ********************************************************************/
+RationalNumber *simplifyRational(RationalNumber *rational, RationalNumber *b)
 {   
-    // parameter n is just for consistency
+    // parameter RationalNumber *b is just for consistency
     int mdc, numerator, denominator;
     RationalNumber *simplify = allocateMemory(sizeof(*simplify));
 
@@ -195,6 +222,14 @@ RationalNumber *simplifyRational(RationalNumber *rational, int n)
     return simplify;
 }
 
+/*********************************************************************
+ *   Function: allocateMemory
+ * ----------------------
+ * Function that receive an size, allocates a pointer to memory with the
+ * specified size and returns a pointer.
+ *
+ * returns: a pointer
+ *  ********************************************************************/
 void *allocateMemory(size_t size)
 {
     void *p = malloc(size);
@@ -206,11 +241,20 @@ void *allocateMemory(size_t size)
     return p;
 }
 
-RationalNumber *getFromUserInput()
+/*********************************************************************
+ *   Function: getFromUserInput
+ * ----------------------
+ * Function that receives a pointer to char, print them to terminal, allocates
+ * a memory address for the number specified by the user and returns a pointer
+ * to this number.
+ *
+ * returns: a pointer
+ *  ********************************************************************/
+RationalNumber *getFromUserInput(char *outputString)
 {   
     int n, d;
   
-    printf("Rational Number A:\n");
+    printf("%s",outputString );
     printf("Numerator:");
     n = readStdinBuffer();
 
@@ -224,7 +268,15 @@ RationalNumber *getFromUserInput()
 
     return rational;
 }
-
+/*********************************************************************
+ *   Function: readStdinBuffer
+ * ----------------------
+ * Function that reads string from user input by reading a char and 
+ * converting to integer. This is used because scanf is a little bit
+ * complicated.
+ * 
+ * returns: integer number
+ *  ********************************************************************/
 int readStdinBuffer()
 {   
     int integer;
@@ -246,42 +298,26 @@ int readStdinBuffer()
     return integer;
 }
 
+/*********************************************************************
+ *   Function: doMMC
+ * ----------------------
+ * Function that performs the MMC to add/subtract number
+ * 
+ * returns: integer number
+ *  ********************************************************************/
 int doMMC(int a, int b)
 {
-    int initValue = 2, aux = 1;
-    bool nextSequence = true;
-    
-    while(true)
-    {
-        if((a % initValue ) && (b % initValue ))
-        {
-            initValue++;
-        }
-        else if (!(a % initValue) && !(b  % initValue))   
-        {
-            return initValue;
-        }
-        else
-        {
-            if (!(a % initValue))
-            {
-                a = a / initValue;
-                aux = aux * initValue
-            }
-            else
-            {
-                b = b / initValue;
-                aux = aux * initValue;
-            }
-        }
-        if (( a == 1) && (b == 1))
-        {
-            break;
-        }
-        
-    }
-    return aux;   
+   
+	return a*b/doMDC(a,b);
 }
+
+/*********************************************************************
+ *   Function: doMDC
+ * ----------------------
+ * Function that performs the MDC to simplify number
+ * 
+ * returns: integer number
+ *  ********************************************************************/
 int doMDC(int a, int b)
 {
     if (b == 0){
@@ -289,8 +325,7 @@ int doMDC(int a, int b)
     }
     else
     {
-        return doMDC(b, a%b)
+        return doMDC(b, a%b);
     }
     
 }
-
