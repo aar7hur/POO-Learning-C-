5d9cadd838461ef8714608064bdc569b1b194d35
aar7hur <munhozarthur@gmail.com>
2020-07-28 20:51:00 -0300

    Added user input verification and class to deal with datetime

--- Lista3/Exercicio3.cpp
+++ Lista3/Exercicio3.cpp
@@ -16,52 +16,33 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <ctime>
+#include <vector>
 
 using namespace std;
-
-typedef enum total_days
-{
-    int jan = 
-    int fev = 
-    int march = 
-    int april = 
-    int m
-}
 
 /*********************************************************
  * 
  *              Class declarations
  * 
  * ******************************************************/
-class User {
-    private:
-        std::string date_of_birth;
-        std::string signo;
-        bool bissextile_year;
-        Datetime date;
-        Horoscope horoscope;
-    
-    public:
-        User(string);
-        std::string get_date_of_birth(void);
-        void set_date_of_birth(string date);
-        bool get_bissextile_year(void);
-        void set_bissextile_year(void);
-        int run_api(void);
-};
 
 class Datetime {
     private:;
         int day;
-        int month;
+        string month;
         int year;
-
+        bool bissextile_year;
+        void set_month_name(int);
+        void set_day(int);
+        void set_year(int);
+        
     public:
         bool validates_parse_date(std::string);
         int converts_date(std::string);
-        void get_day(void);
-        void get_month(void);
-        void get_year(void);
+        int get_day(void);
+        std::string get_month(void);
+        int get_year(void);
 };
 
 class Horoscope {
@@ -72,36 +53,161 @@ class Horoscope {
         std::string get_sign(void);
 };
 
+class User {
+    private:
+        Datetime date;
+        Horoscope horoscope;
+        void print_user(void);
+    public:
+        int run_api(string);
+};
+
+
 /*********************************************************
  * 
  *              User Class methods
  * 
  * ******************************************************/
-User::User(string date)
+int User::run_api(string date)
 {
-    this->set_date_of_birth(date);
+    this->date.validates_parse_date(date);
+    this->print_user();
+} 
+
+void User::print_user(void)
+{
+    cout << this->date.get_day() << " de " << this->date.get_month();
+    cout << " de " << this->date.get_year() << "\n";
 }
 
-bool User::get_bissextile_year()
-{
-    return bissextile_year;
-}
-
-std::string User::get_date_of_birth()
-{
-    return date_of_birth;
-}
 /*********************************************************
  * 
  *              Datetime Class methods
  * 
  * ******************************************************/
+void Datetime::set_year(int year)
+{
+    time_t t = time(NULL);
+    struct tm* tPtr = localtime(&t);
+
+    if ((year > 1800) && (year < (tPtr->tm_year + 1900)))
+    {
+        this->year = year;
+       
+    }
+    else
+    {
+        cout << "The year must be a number betwee 1800 and 2020!!";
+        exit(EXIT_FAILURE);
+    }
+
+    if (((year % 4 == 0) && (year %100 != 0)) || (year % 400 == 0))
+    {
+        this->bissextile_year = true;
+    }
+}
+
+void Datetime::set_month_name(int month)
+{
+    const std::vector<std::string> months_of_year = {
+        "Janeiro","Fevereiro","Março",
+        "Abril","Maio","Junho","Julho",
+        "Agosto","Setembro","Outubro",
+        "Novembro","Dezembro",
+    };
+
+    if ((month > 0) && (month <= 12))
+    {
+        this->month = months_of_year[month-1];
+    }
+    else
+    {
+        cout << "The month must be a number betwee 1 and 12!!";
+        exit(EXIT_FAILURE);
+    }
+}
+
+void Datetime::set_day(int day)
+{
+    std::map<std::string, int> months_dict;
+
+    months_dict["Janeiro"] = 31;  
+    months_dict["Fevereiro"] = 28;
+    months_dict["Março"] = 31;  
+    months_dict["Abril"] = 30;
+    months_dict["Maio"] = 31;
+    months_dict["Junho"] = 30;
+    months_dict["Julho"] = 31;
+    months_dict["Agosto"] = 30;
+    months_dict["Setembro"] = 30;
+    months_dict["Outubro"] = 31;
+    months_dict["Novembro"] = 30;
+    months_dict["Dezembro"] = 31;
+
+    if (this->bissextile_year)
+    {
+        months_dict["Fevereiro"] = 29;
+    }
+
+    if ((day >= 1) && (day <= months_dict[this->month]))
+    {
+        this->day = day;
+    }
+    else
+    {
+        cout << "Please enter with a valid day!";
+        exit(EXIT_FAILURE);
+    } 
+}
+
+int Datetime::get_year(void)
+{
+    return this->year;
+}
+
+std::string Datetime::get_month(void)
+{
+    return this->month;
+}
+
+int Datetime::get_day(void)
+{
+    return this->day;
+}
 
 bool Datetime::validates_parse_date(const std::string date)
 {
-    std::string day = date.substr(0,2);
-    std::string month = date.substr(2,2);
-    std::string year = date.substr(4,4);
 
-    
+    std::size_t delimiter_first_of = date.find('/');
+    std::size_t delimiter_last_of = date.rfind('/');
+  
+    if (delimiter_first_of == std::string::npos)
+    {
+        cout << "Please enter with date in format dd/mm/AAAA";
+        exit(EXIT_FAILURE);
+    }
+    if (delimiter_last_of == std::string::npos)
+    {
+        cout << "Please enter with date in format dd/mm/AAAA";
+        exit(EXIT_FAILURE);
+    }
+
+    int day = stoi(date.substr(0, delimiter_first_of));
+    int month = stoi(date.substr(delimiter_first_of+1, 2));
+    int year = stoi(date.substr(delimiter_last_of+1, 4));
+
+    this->set_month_name(month);
+    this->set_year(year);
+    this->set_day(day);
+}
+
+
+int main()
+{
+    std::string str;
+
+    cout << "Please enter with yout birthday date: ";
+    getline(cin, str);
+    User User;
+    User.run_api(str);
 }
