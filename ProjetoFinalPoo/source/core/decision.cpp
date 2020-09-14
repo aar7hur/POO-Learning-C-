bb261c1c399accb80538d1d2da799cbc5654954c
Gustavo Godoy <grrgodoy@gmail.com>
2020-09-13 21:04:41 -0300

    Update decision.cpp

--- ProjetoFinalPoo/source/core/decision.cpp
+++ ProjetoFinalPoo/source/core/decision.cpp
@@ -2,6 +2,7 @@
 #include "../include/readcsv.h"
 #include "../include/movingAverage.h"
 #include <string>
+#include <algorithm>
 
 /*******************************************************************************
  *	Função: populateData
@@ -19,28 +20,31 @@ void Decision::populateData(std::string ativo){
         this->populateCloseWeek(ativo);
         this->populateStochastic(ativo);
     }
-    catch(...)
+    catch(const char* msg)
     {
+        std::cerr << msg << std::endl;
         std::cout << "Erro! Não existe esse ativo no sistema";
         exit(-1);
     }
 }
 void Decision::populateAverage(std::string ativo){
 
-    
-    Tabela meuAtivo(ativo, "w");
+    Ativo meuAtivo(ativo, "w");
     float *array;
     meuAtivo["Close"] >> array;
-
+    
     this->movingAverage.setmovingAverage(array, 20);
     this->decisionData.average_20 = this->movingAverage.getAverage();
 
     this->movingAverage.setmovingAverage(array, 50);
     this->decisionData.average_50 = this->movingAverage.getAverage();
+    
+    // Foi testado aqui, lendo e calculando average corretamente, mexer daqui pra frente
+    
 }
 void Decision::populateCloseWeek(std::string ativo)
 {
-    Tabela meuAtivoClosed(ativo, "w");
+    Ativo meuAtivoClosed(ativo, "w");
     float *arrayClose;
     meuAtivoClosed["Close"] >> arrayClose; //ver quantas posições retorna no array
     this->decisionData.closeWeek = arrayClose[0];
@@ -49,18 +53,32 @@ void Decision::populateCloseWeek(std::string ativo)
 
 void Decision::populateStochastic(std::string ativo)
 {
-    Tabela meuAtivoSto(ativo, "d");
-    float *arraySto;
-
-    meuAtivoSto["Close"] >> arraySto; //ver quantas posições retorna no array
-    this->stochastic.setPrice(arraySto);
-    this->stochastic.getHighDailyPrice();
-    this->stochastic.getLowDailyPrice();
-    this->stochastic.getKcurve();
-    this->stochastic.averageKcurve();
-    this->decisionData.stochastic_8 = this->stochastic.getResult();
-    this->decisionData.highDaily = this->stochastic.getHighDailyPrice();
-    this->decisionData.lowDaily = this->stochastic.getLowDailyPrice();
+    enum {magic_periodo = 8}; // Máximo de dados do CSV que devem ser usados
+    
+    // Le a coluna de Close
+    Ativo meuAtivoSto(ativo, "d");
+    
+    // Popula arrayClose
+    float *arrayClose; size_t sizeClose;
+    sizeClose = meuAtivoSto["Close"](magic_periodo) >> arrayClose;
+    float closeMaisRecente = arrayClose[0];
+    
+    // Ler Baixas
+    float *arrayLow; size_t sizeLow;
+    sizeLow = meuAtivoSto["Low"](magic_periodo) >> arrayLow;
+    float minimaDoPeriodo = *( std::min_element(arrayLow, &arrayLow[sizeLow]) );
+    
+    // Ler Altas
+    float *arrayHigh; size_t sizeHigh;
+    sizeHigh = meuAtivoSto["High"](magic_periodo) >> arrayHigh;
+    float maximaDoPeriodo = *( std::min_element(arrayHigh, &arrayHigh[sizeHigh]) );
+    
+    // Calcular %K
+    float kValue = 100*( closeMaisRecente - minimaDoPeriodo)/(maximaDoPeriodo - minimaDoPeriodo);
+    
+    this->decisionData.lowDaily = minimaDoPeriodo;
+    this->decisionData.highDaily = maximaDoPeriodo;
+    this->decisionData.stochastic_8 = kValue;
 }
 
 /*******************************************************************************
@@ -72,9 +90,10 @@ void Decision::populateStochastic(std::string ativo)
  * OBS: Essa função será chamada uma vez apenas após pupulateData()
  ******************************************************************************/
 
-calculation Decision::doDecision(void)
+
+Decision::calculation Decision::doDecision(void)
 {   
-    this->calculation_process = WAITING_FOR_CALCULATION;
+    this->calculation_process = Decision::WAITING_FOR_CALCULATION;
 
     if(this->isPurchaseAction() == true)
     {
@@ -85,8 +104,9 @@ calculation Decision::doDecision(void)
         this->purchaseAction = false;
     }
 
-    this->managmentRisk();
-    this->calculation_process = CALCULATION_IS_OVER;
+    this->managementRisk();
+    
+    this->calculation_process = Decision::CALCULATION_IS_OVER;
 
     return this->calculation_process;
     
@@ -102,16 +122,13 @@ calculation Decision::doDecision(void)
  ******************************************************************************/
 bool Decision::isSaleAction(void)
 {
-    if(this->decisionData.closeWeek < this->decisionData.average_20)
-    {
-         if(this->decisionData.average_20 < this->decisionData.average_50)
-         {
-            if(this->decisionData.stochastic_8 > MAX_STOCHASTIC_SALE) 
-            {
-                return true;
-            }
-         }
-    }
+    if( // Verdadeiro se todas estas condições são verdadeiras:
+        (this->decisionData.closeWeek < this->decisionData.average_20) &&
+        (this->decisionData.average_20 < this->decisionData.average_50) &&
+        (this->decisionData.stochastic_8 > MAX_STOCHASTIC_SALE)
+    )  {return true;}
+      
+      return false;
 }
 
 /*******************************************************************************
@@ -124,16 +141,13 @@ bool Decision::isSaleAction(void)
  ******************************************************************************/
 bool Decision::isPurchaseAction(void)
 {
-    if(this->decisionData.closeWeek >= this->decisionData.average_20)
-    {
-         if(this->decisionData.average_20 >= this->decisionData.average_50)
-         {
-            if(this->decisionData.stochastic_8 <= MAX_STOCHASTIC_PURCHASE) 
-            {
-                return true;
-            }
-         }
-    }
+    if( // Verdadeiro se todas estas condições são verdadeiras:
+        (this->decisionData.closeWeek >= this->decisionData.average_20) &&
+        (this->decisionData.average_20 >= this->decisionData.average_50) &&
+        (this->decisionData.stochastic_8 <= MAX_STOCHASTIC_PURCHASE)
+    ) {return true;}
+    
+    return false;
 }
 
 /*******************************************************************************
@@ -145,38 +159,43 @@ bool Decision::isPurchaseAction(void)
  *	retorna: nada
  ******************************************************************************/
 
-void Decision::managmentRisk()
+void Decision::managementRisk()
 {
     int target, qtdStocks = 1;
     float stopLoss, trigger, riskTrade;
+    	
     
-
-    stopLoss = this->decisionData.lowDaily;
-	trigger = this->decisionData.highDaily;
-
-    if(this->purchaseAction = true)
+    if(this->purchaseAction == true)
     {
+        stopLoss = this->decisionData.lowDaily;
+        trigger = this->decisionData.highDaily;
         target = ((trigger - stopLoss) * 3) + trigger;
+        
 	    while(riskTrade < (0.01*this->userMoney)){
                 
             riskTrade = (trigger - stopLoss) * qtdStocks;
-            qtdStocks++;	
+            qtdStocks++;
         }
-        this->outputUser.message  = "Compra";
+       this->outputUser.message  = "Compra";
 	}
 
-    else if(this->purchaseAction = false)
+    else if(this->purchaseAction == false)
     {
+        stopLoss = this->decisionData.highDaily;
+        trigger = this->decisionData.lowDaily;
         target = trigger - ((stopLoss - trigger) * 3);
+		
 		while(riskTrade < (0.01*this->userMoney)){
 			riskTrade = (stopLoss - trigger) * qtdStocks;
-			qtdStocks++;	
+			qtdStocks++;
+            
 		}
-		this->outputUser.message  = "Venda";
+	     this->outputUser.message  = "Venda";
     }
 
     this->outputUser.stopLoss = stopLoss;
     this->outputUser.trigger = trigger;
+    this->outputUser.target = target;
     this->outputUser.qtdStocks = qtdStocks;
 }
 
