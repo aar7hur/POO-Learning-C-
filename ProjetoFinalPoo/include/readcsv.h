517b68d35f0e5a7dc894f51142ca23fed3baa79a
Gustavo Godoy <grrgodoy@gmail.com>
2020-09-13 21:01:44 -0300

    Atualizar readcsv.h

--- ProjetoFinalPoo/include/readcsv.h
+++ ProjetoFinalPoo/include/readcsv.h
@@ -22,24 +22,65 @@ class Coluna{
 
     Coluna& operator<<(std::string entrada);
 
+    template <class Tipo, class Func>
+    size_t jogarPraArray(Tipo *&array, Func converter, size_t limite=0);
+    
+    std::string topo();
+    
+};
+
+class Coluna_Interface{
+    private: Coluna * coluna;
+    public: size_t requestSize;
+    
+    public:
+    Coluna_Interface(Coluna *origem, size_t size=0);
+    Coluna_Interface & operator()(size_t size);
     size_t operator>>(std::string *&array);
     size_t operator>>(int *&array);
     size_t operator>>(float *&array);
     size_t operator>>(double *&array);
+    
+    std::string topo();
+    template<class T> T topo();
 };
 
+// Functor helpers
+struct _transparente;
+struct _stoi;
+struct _stof;
+struct _stod;
 
 class Tabela{
-    private:
+
+protected:
     std::vector<std::string> headers;
     std::vector<Coluna> colunas;
+    Tabela(); // Construtor vazio, penas para heranças.
+        
+public:
+ 
+    Tabela(std::string arquivo);
     
-    public:
-    Tabela(std::string acao, std::string tipo="d");
+    virtual Tabela& open(std::string arquivo);
     
     Tabela& operator<<(std::vector<std::string> linha);
    
-    Coluna& operator[](std::string header);
+    Coluna_Interface operator[](std::string header);
+};
+
+
+class Ativo: public Tabela{
+
+public:
+    
+    Ativo(std::string acao, std::string tipo="d");
+    Ativo& open(std::string acao, std::string tipo="d");
+    
+    // Operadores precisam ser redefinidos em uma herança
+    Ativo& operator<<(std::vector<std::string> linha);
+    Coluna_Interface operator[](std::string header);
+    
 };
 
 #endif
